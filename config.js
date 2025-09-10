cbc35eb4cdfba309b91e36c2b4a24041d2b6a6f2
Sjsidhsjsksks <computerekakalupatai@gmail.com>
2025-09-10 13:17:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID: 'md9SkTBQ#w6Hdi7us2GSdMBWtspVmXqNY85lKPLG9Q7D3AUMhOo0',
+SESSION_ID: 'GAMEAIaA#XCdfT3WRanlWlIm2np-m7ox9ExY64YRF_omtbx_pAZM',
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 MV_BLOCK: process.env. MV_BLOCK === undefined ? 'false' : process.env. MV_BLOCK,    
 ANTI_LINK: process.env.ANTI_LINK === undefined ? 'true' : process.env.ANTI_LINK, 
