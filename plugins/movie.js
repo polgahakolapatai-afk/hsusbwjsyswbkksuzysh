3460d5519b2c505cd43fdf0a102046482e4701a2
polgahakolapatai-afk <polgahakolapatai@gmail.com>
2025-09-12 22:27:22 +0530

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -1247,7 +1247,7 @@ cmd({
 
                 if (check?.isUploaded === false) {
                     // New upload case
-                    const urlApi = `https://manojapi.infinityapi.org/api/v1/cinesubz-download?url=${encodeURIComponent(url)}&apiKey=sadasthemi20072000`; 
+                    const urlApi = `https://manojapi.infinityapi.org/api/v1/cinesubz-download?url=${encodeURIComponent(url)}&apiKey=94646895-793d-4a91-973d-f6eb181d6a61`; 
                     const getDownloadUrls = await axios.get(urlApi);
 
                     downloadUrls = getDownloadUrls.data.results;
